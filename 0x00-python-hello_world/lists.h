4d39f09c3c771d206e598531716ea569765b1af5
tshepithekiso <tshepangthekiso15@gmail.com>
2023-11-27 08:31:35 -0800

    10-check_cycle.c,

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_t - linked lists
- * @n: numbers
- * @next: next lists
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: linked list structure
+ * Description: singly linked list node structure
+ * 
  */
-
-typedef struct listint_t
+typedef struct listint_s
 {
-	int n;
-	struct listint_t *next;
-}listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
