d89b95a40868fd8706b381613c79a30ee76c617a
tshepithekiso <tshepangthekiso15@gmail.com>
2023-11-28 04:12:49 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
