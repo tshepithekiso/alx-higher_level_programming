9f725c70848e79abd5d299d3ec0b46dfad1810fd
tshepithekiso <tshepangthekiso15@gmail.com>
2023-11-28 04:07:48 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,26 +7,26 @@
  *
  * Return: NULL
  */
-listint_t *inside_node(listint_t **head, int number)
+listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new ;
+	listint_t *node = *head, *new;
 
-	new = malloc(sizeof(listint_t)) ;
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-		return (NULL) ;
-	new->n = number ;
+		return (NULL);
+	new->n = number;
 
-	if (node == NULL || node->n>=number)
+	if (node == NULL || node->n >= number)
 	{
-		new->next = node ;
-		*head = new ;
-		return (new) ;
+		new->next = node;
+		*head = new;
+		return (new);
 	}
 
-	while (node && node->next && node->next->n<number)
-		node = node->next ;
-	new->next = node->next ;
-	node->next = new ;
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+	new->next = node->next;
+	node->next = new;
 
-return (new) ;
+	return (new);
 }
