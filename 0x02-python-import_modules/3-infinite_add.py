4e35ec36a71e0a91885ca31ff82c87c6bfa42469
tshepithekiso <tshepangthekiso15@gmail.com>
2023-11-30 07:32:48 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
 
     result = 0
-    for i in range(len(sys.argv) - 1):
-        result += int(sys.argv[i + 1])
+    for i in sys.argv:
+        result += int()
         print("{}".format(result))
