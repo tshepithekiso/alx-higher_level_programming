ab646478457c48370243682c2fce3b0adac59150
tshepithekiso <tshepangthekiso15@gmail.com>
2024-05-02 11:13:13 +0000

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// use document.querySelector to select the HTML tag
+document.addEventListener('DOMContentLoaded', function() {
+    var header = document.querySelector('header');
+    header.style.color = '#FF0000';
+});
+
